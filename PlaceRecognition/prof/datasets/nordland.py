78e1096271e5e6a8aad84d5d38b4bb2f981a7a1f
Sangyun Lee <sylee@skhu.ac.kr>
2024-04-12 14:28:22 +0900

    fix bug in nordland sampling

--- PlaceRecognition/prof/datasets/nordland.py
+++ PlaceRecognition/prof/datasets/nordland.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import torch
 from natsort import natsorted
 import glob
@@ -6,38 +7,69 @@ import os
 import numpy as np
 from skimage import io
 
-class CustomDataset(torch.utils.data.Dataset): 
-  def __init__(self, config, data_path):
-    self.window = config.window
-    self.img_h = config.img_h
-    self.img_w = config.img_w
-    self.batch_size = config.batch_size
-    self.seed = config.seed
-    self.data_path = data_path
-    self.img_list = glob.glob(os.path.join(self.data_path, "*/*/*.png"))
-    self.img_list = natsorted(self.img_list)
-    self.pos_list = []
-    for idx, img in enumerate(self.img_list):
-        temp = []
-        for i in range(max(0, idx-self.window), min(idx+self.window+1, len(self.img_list))):
-            if (i != idx) and (img.split("/")[-2] == self.img_list[i].split("/")[-2]):
-                temp.append(self.img_list[i])
-        self.pos_list.append(temp)
-        
-  def __len__(self):
-    return len(self.img_list)
 
-  def __getitem__(self, idx): 
-    anc_path = self.img_list[idx]
-    pos_path = np.random.choice(self.pos_list[idx])
-    while True:
-        i = random.randrange(len(self.img_list))
-        if (i != idx) and (self.img_list[i] not in self.pos_list[idx]):
-            neg_path = self.img_list[i]
-            break
+class CustomDataset(torch.utils.data.Dataset):
+    def __init__(self, config, data_path):
+        self.window = config.window
+        self.img_h = config.img_h
+        self.img_w = config.img_w
+        self.batch_size = config.batch_size
+        self.seed = config.seed
+        self.data_path = data_path
 
-    anc = io.imread(anc_path)
-    pos = io.imread(pos_path)
-    neg = io.imread(neg_path)
-    return anc, pos, neg
-           
+        self.images = glob.glob(os.path.join(self.data_path, "*/*/*.png"))
+        self.seasons = [(f.path, f.name) for f in os.scandir(data_path) if f.is_dir()]
+        self.sections = [(f.path, f.name) for f in os.scandir(self.seasons[0][0]) if f.is_dir()]
+        self.maxnum = {}
+        for path, name in self.sections:
+            files = natsorted(glob.glob(os.path.join(path, "*.png")))
+            self.maxnum[name] = [int(os.path.splitext(os.path.basename(files[0]))[0]), int(os.path.splitext(os.path.basename(files[-1]))[0])]
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        anc_path = self.images[idx]
+        pos_path = self.get_positive(anc_path)
+        neg_path = self.get_negative(anc_path)
+        anc = io.imread(anc_path)
+        pos = io.imread(pos_path)
+        neg = io.imread(neg_path)
+
+        return anc, pos, neg
+
+    def get_positive(self, path):
+        season_src = path.split("/")[-3]
+        section_src = path.split("/")[-2]
+        number_src = int(os.path.splitext(path.split("/")[-1])[0])
+        ext = os.path.splitext(path.split("/")[-1])[1]
+
+        while True:
+            season_dst = random.choice(self.seasons)[1]
+            number_dst = random.randrange(max(self.maxnum[section_src][0], number_src-self.window), min(number_src+self.window+1, self.maxnum[section_src][1]))
+            if season_src != season_dst or number_src != number_dst:
+                break
+
+        path = path.replace(season_src, season_dst)
+        path = path.replace(str(number_src)+ext, str(number_dst)+ext)
+
+        return path
+
+    def get_negative(self, path):
+        season_src = path.split("/")[-3]
+        section_src = path.split("/")[-2]
+        number_src = int(os.path.splitext(path.split("/")[-1])[0])
+        ext = os.path.splitext(path.split("/")[-1])[1]
+
+        while True:
+            season_dst = random.choice(self.seasons)[1]
+            section_dst = random.choice(self.sections)[1]
+            number_dst = random.randrange(self.maxnum[section_dst][0], self.maxnum[section_dst][1]+1)
+            if season_src != season_dst or section_src != section_dst or abs(number_src - number_dst) > self.window:
+                break
+
+        path = path.replace(season_src, season_dst)
+        path = path.replace(section_src, section_dst)
+        path = path.replace(str(number_src)+ext, str(number_dst)+ext)
+
+        return path
